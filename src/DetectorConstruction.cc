09cc00557d180916eb2f0e6dc536834b04e95ae8
Felix Paul Georg Stehr <felix.paul.georg.stehr@desy.de>
2021-06-22 11:18:49 +0200

    test2

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -119,6 +119,7 @@ void DetectorConstruction::DefineMaterials()
 
 
   G4Element* C      = new G4Element(name="Carbon" ,symbol="C"  , z= 6 , a =   12*g/mole);
+  G4Element* H     = new G4Element(name="Hydrogen" ,symbol="H"  , z= 1 , a =   2*g/mole);
 
 
   // definde materials
